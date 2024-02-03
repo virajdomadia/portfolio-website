b85092db42705f47129026213d593c3bb3fd0bc6
Viraj <virajdomadia32@gmail.com>
2024-02-03 16:29:16 +0530

    updated html css and js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let typed = new Typed(".auto-type", {
-    strings: ["Front-End Developer", "JavaScript Developer", "Web Developer"],
-    typeSpeed: 75,
-    backSpeed: 75,
-    loop: true
+  strings: ["Front-End Developer", "JavaScript Developer", "Web Developer"],
+  typeSpeed: 75,
+  backSpeed: 75,
+  loop: true,
 });
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxHtG00OvwOWpsez0Nx-iBKg3ky0dnpv5pDdd1DqvvmbO3uZ1dgYZIhWn-QGG0CZ3BtTw/exec'
-  const form = document.forms['submit-to-google-sheet']
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxHtG00OvwOWpsez0Nx-iBKg3ky0dnpv5pDdd1DqvvmbO3uZ1dgYZIhWn-QGG0CZ3BtTw/exec";
+const form = document.forms["submit-to-google-sheet"];
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
